7a9070da1bada2e94f5d910e58255fc1a3bf346e
Amrr-Maherr <amrr.maherr242gmail.com>
2025-11-30 21:17:35 +0200

    Refactor app layout and update home page
    Simplify structure and global styles in layout.tsx.
    Adjust dashboard content and responsive layout in page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col lg:flex-row">
-      <div className="w-full h-screen lg:w-1/2 flex flex-col justify-center items-center p-10">
-        <h1 className="text-4xl font-bold mb-6">Welcome to the Dashboard</h1>
+      <div className="w-full h-screen lg:w-1/2 flex flex-col justify-center items-center p-10 text-center">
+        <h1 className="text-4xl font-bold mb-6">
+          Welcome to{" "}
+          <span className="bg-[#6768fa] text-white px-2 py-1 rounded-lg font-semibold">
+            StoreHub
+          </span>
+        </h1>
         <p className="mb-4 text-gray-700 text-lg">
           Here you can manage products, orders, and customers easily.
         </p>
 
-        <div className="flex gap-4 mt-6">
+        <div className="flex md:flex-nowrap flex-wrap items-center justify-center gap-4 mt-6">
           <Link href="/Login" className="cursor-pointer">
-            <Button className="cursor-pointer"> Manage Products</Button>
+            <Button className="cursor-pointer"> Start Managing Products</Button>
           </Link>
           <Link href="/Login" className="cursor-pointer">
             <Button variant="outline" className="cursor-pointer">
-              Orders
+              Check Your Orders
             </Button>
           </Link>
         </div>
